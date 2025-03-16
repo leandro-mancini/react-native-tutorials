53d8d6d99aa49709d729af55cd8d46cf093e9cbe
leandro-mancini <mancinilele@gmail.com>
2025-03-16 10:01:12 -0300

    feat(dokterian): update splash screen

--- Dokterian/app/splash.tsx
+++ Dokterian/app/splash.tsx
@@ -1,91 +1,52 @@
-import { useEffect, useRef } from "react";
-import { Animated, Easing, StyleSheet, View } from "react-native";
+import React, { useEffect } from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import LottieView from "lottie-react-native";
+
+const { width, height } = Dimensions.get("window");
 
 export const SplashScreen = ({ navigation }: any) => {
-  const logoScale = useRef(new Animated.Value(0)).current;
-  const logoOpacity = useRef(new Animated.Value(0)).current;
-  const pulseAnimation = useRef(new Animated.Value(1)).current;
-  const textOpacity = useRef(new Animated.Value(0)).current;
-
   useEffect(() => {
-    Animated.sequence([
-        // Fade-in do logo
-        Animated.timing(logoOpacity, {
-            toValue: 1,
-            duration: 800,
-            useNativeDriver: true,
-        }),
-        
-        // Expansão inicial do logo com bounce
-        Animated.spring(logoScale, {
-            toValue: 1,
-            friction: 4,
-            tension: 40,
-            useNativeDriver: true,
-        }),
-
-        Animated.timing(textOpacity, {
-          toValue: 1,
-          duration: 1000,
-          useNativeDriver: true,
-        }),
-        
-        // Pulso animado simulando batimentos cardíacos
-        Animated.loop(
-            Animated.sequence([
-                Animated.timing(pulseAnimation, {
-                    toValue: 1.1,
-                    duration: 500,
-                    easing: Easing.ease,
-                    useNativeDriver: true,
-                }),
-                Animated.timing(pulseAnimation, {
-                    toValue: 1,
-                    duration: 500,
-                    easing: Easing.ease,
-                    useNativeDriver: true,
-                })
-            ]),
-            { iterations: 3 } // Repete a animação 3 vezes
-        ),
-    ]).start(() => {
-        setTimeout(() => {
-            navigation.replace("Main");
-        }, 1000);
-    });
+    setTimeout(() => {
+      navigation.replace("Main"); // Redireciona para a tela principal
+    }, 3000); // Tempo da animação
   }, [navigation]);
 
   return (
     <View style={styles.container}>
-        <Animated.Image
-            source={require("../assets/images/logo.png")}
-            style={[styles.logo, {
-                opacity: logoOpacity,
-                transform: [{ scale: logoScale }, { scale: pulseAnimation }],
-            }]}
-        />
-        <Animated.Text style={[styles.text, { opacity: textOpacity }]}>Dokterian</Animated.Text>
+      {/* Animação de fundo */}
+      <LottieView
+        source={require("../assets/animations/background.json")}
+        autoPlay
+        loop
+        resizeMode="cover"
+        style={styles.backgroundAnimation}
+      />
+
+      {/* Animação do logotipo */}
+      <LottieView
+        source={require("../assets/animations/logotipo.json")}
+        autoPlay
+        loop
+        style={styles.logoAnimation}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "white",
-      gap: 16
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#E3F2FD",
   },
-  logo: {
-      width: 70,
-      height: 70,
-      resizeMode: "contain",
+  backgroundAnimation: {
+    position: "absolute",
+    width: width,
+    height: height,
   },
-  text: {
-    color: "#0D1B34",
-    fontSize: 20,
-    lineHeight: 20,
-    fontFamily: "Poppins-Bold"
+  logoAnimation: {
+    width: 200,
+    height: 200,
   },
 });
