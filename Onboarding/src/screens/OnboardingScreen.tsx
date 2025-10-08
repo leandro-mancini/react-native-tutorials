da57b53f0bbee6173c619c96095a9c2bf5ab9ce9
leandro-mancini <mancinilele@gmail.com>
2025-10-08 15:01:41 -0300

    feat(onboarding): update onboarding screen

--- Onboarding/src/screens/OnboardingScreen.tsx
+++ Onboarding/src/screens/OnboardingScreen.tsx
@@ -13,6 +13,11 @@ import { SLIDES } from '../constants/onboardingSlides';
 import type { Slide, CommonSlideProps } from '../components/onboarding/types';
 import { SlideItem } from '../components/onboarding/SlideItem';
 
+import Figure1Step1 from '../../assets/svg/figure1-step1.svg';
+import Figure2Step1 from '../../assets/svg/figure2-step1.svg';
+import Figure3Step1 from '../../assets/svg/figure3-step1.svg';
+import Figure4Step3 from '../../assets/svg/figure4-step3.svg';
+
 const { width } = Dimensions.get('window');
 
 export default function OnboardingScreen({ navigation }: any) {
@@ -40,7 +45,11 @@ export default function OnboardingScreen({ navigation }: any) {
   return (
     <Animated.View style={[styles.container, bgStyle]}>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1, position: 'relative' }}>
+        <Figure1Step1 width={111} height={175} color="#F4C9FF" style={{ position: 'absolute', top: 90, left: -50 }} />
+        <Figure2Step1 width={213.29} height={93.1} color="#DF61FF" style={{ position: 'absolute', top: -40, left: 0 }} />
+        <Figure3Step1 width={70.49} height={150.37} color="#C180F4" style={{ position: 'absolute', top: 20, right: 0, transform: [{ scale: 1.4 }] }} />
+        <Figure4Step3 width={310} height={286} color="#FF7171" style={{ position: 'absolute', top: 200, right: -165, transform: [{"scale":0.6}] }} />
         <Animated.FlatList
           ref={pagerRef}
           data={SLIDES}
