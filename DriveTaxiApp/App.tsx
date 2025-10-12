a1c3471b51e2a299cd29efc24093a9918b0909d8
leandro-mancini <mancinilele@gmail.com>
2025-10-12 11:14:42 -0300

    feat(drive-taxi): resolved warning safe area context

--- DriveTaxiApp/App.tsx
+++ DriveTaxiApp/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NextScreen } from './src/screens/NextScreen';
 import { WelcomeScreen } from './src/screens/WelcomeScreen';
 
@@ -13,11 +14,13 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Next" component={NextScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+          <Stack.Screen name="Next" component={NextScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
