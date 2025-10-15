16fb8407307a1890fb956a8a2956d180f4851d70
leandro-mancini <mancinilele@gmail.com>
2025-10-15 14:32:46 -0300

    feat(drive-taxi): update app

--- DriveTaxiApp/src/screens/HomeScreen.tsx
+++ DriveTaxiApp/src/screens/HomeScreen.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export function HomeScreen() {
-  return (
-    <View style={styles.c}>
-      <Text style={styles.t}>Tela Inicial 🚀</Text>
+   return (
+    <View style={styles.container}>
+      <MapView style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialCamera={{
+          center:{
+            latitude: 37.78825,
+            longitude: -122.4324,
+          },
+          pitch: 0,
+          heading: 0,
+          altitude: 1000,
+          zoom: 16,
+          
+        }}
+      />
     </View>
-  );
+   );
 }
-const styles = StyleSheet.create({
-  c: { flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#111' },
-  t: { color: '#fff', fontSize: 22, fontWeight: '600' },
-});
