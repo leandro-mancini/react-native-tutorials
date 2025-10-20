067d56997403b911ea85bbe22279b06b3280d510
leandro-mancini <mancinilele@gmail.com>
2025-10-20 20:32:51 -0300

    feat(lock-flow): update app

--- LockFlow/src/DashboardScreen.tsx
+++ LockFlow/src/DashboardScreen.tsx
@@ -4,11 +4,7 @@ import { View, Text, StyleSheet, Pressable } from 'react-native';
 export function DashboardScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>✅ Desbloqueado</Text>
-      <Text style={styles.subtitle}>Bem-vindo ao app!</Text>
-      <Pressable style={styles.button} onPress={() => {}}>
-        <Text style={styles.buttonText}>Ação Qualquer</Text>
-      </Pressable>
+      <Text style={styles.title}>Olá 👋, eu sou Mancini!</Text>
     </View>
   );
 }
@@ -16,7 +12,4 @@ export function DashboardScreen() {
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: '#0D80FF', alignItems: 'center', justifyContent: 'center' },
   title: { color: 'white', fontSize: 26, fontWeight: '700', marginBottom: 8 },
-  subtitle: { color: 'white', opacity: 0.9, fontSize: 16, marginBottom: 20 },
-  button: { backgroundColor: 'white', paddingHorizontal: 16, paddingVertical: 10, borderRadius: 12 },
-  buttonText: { color: '#0D80FF', fontWeight: '700' },
 });
